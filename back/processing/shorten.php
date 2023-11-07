6e83fdd59abf36a534b847c613aee8540d1501aa
maelbadet <maelbadet21@gmail.com>
2023-11-07 21:19:17 +0100

    fix: pushing in bdd

--- back/processing/shorten.php
+++ back/processing/shorten.php
@@ -1,43 +1,30 @@
 <?php
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $originalLink = $_POST["original_link"];
 
-if (isset($_GET["short"])) {
-    
-    $shortLink = $_GET["short"];
+    // Générer un raccourci aléatoire avec random_bytes
+    $shortLink = generateRandomShortLink();
 
-    // Effectuez une recherche dans la base de données pour trouver le lien d'origine associé au raccourci
+    // Vous pouvez enregistrer le lien d'origine et le raccourci dans une base de données
     // Remplacez les détails de la base de données par les vôtres
     $database = new mysqli("localhost", "root", "", "gofast");
     
-    // Utilisation d'une requête préparée
-    $query = "SELECT link FROM url WHERE link_rewrite = ?";
-    $stmt = $database->prepare($query);
-    
-    if ($stmt) {
-        // Lier la valeur du raccourci
-        $stmt->bind_param("s", $shortLink);
-        $stmt->execute();
-        $stmt->store_result();
+    $query = "INSERT INTO url (link, link_rewrite) VALUES ('$originalLink', '$shortLink')";
+    $result = $database->query($query);
 
-        if ($stmt->num_rows > 0) {
-            $stmt->bind_result($originalLink);
-            $stmt->fetch();
-
-            // Redirigez l'utilisateur vers le lien d'origine
-            http_response_code(301);
-            header("Location: $originalLink");
-            exit;
-        } else {
-            // Gérer le cas où le raccourci est introuvable dans la base de données
-            http_response_code(404);
-            echo "Lien raccourci invalide.";
-        }
-        
-        $stmt->close();
+    if ($result) {
+        // Afficher le résultat
+        echo "Lien d'origine : <a href=\"$originalLink\" target=\"_blank\">$originalLink</a><br>";
+        echo "Lien raccourci : <a href=\"redirect.php?short=$shortLink\" target=\"_blank\">$shortLink</a>";
     } else {
-        // Gérer l'erreur de préparation de la requête
-        http_response_code(500);
-        echo "Erreur de préparation de la requête.";
+        // Gérer le cas où l'enregistrement en base de données échoue
+        echo "Erreur lors de l'enregistrement du lien.";
     }
 }
 
+function generateRandomShortLink() {
+    $length = 7; // Longueur du raccourci
+    $bytes = random_bytes($length);
+    return bin2hex($bytes);
+}
 ?>
