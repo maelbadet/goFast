997df79fd774cd28269bafbb1135515fe08bd3ad
maelbadet <maelbadet21@gmail.com>
2023-11-09 15:58:53 +0100

    fix: add session to crud

--- front/partials/header.php
+++ front/partials/header.php
@@ -1,5 +1,9 @@
 <?php 
 // le $root_url est a changer par l'url root dans votre navigateur (celui qui s'arrete au nom du projet)
+
+session_start();
+$_SESSION['id']= 1;
+
 $root_url = "http://localhost/LiveCampus/php/cours_php_2/goFast/";
 $lien = "front/vue/mainPage.php";
 $liens = [
