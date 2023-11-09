9a997c04181d29d1749ef61a235fc6c27a4e7ff6
maelbadet <maelbadet21@gmail.com>
2023-11-09 16:55:17 +0100

    fix: add js and stylistic to crud and navbar$

--- front/partials/nav.php
+++ front/partials/nav.php
@@ -1,9 +1,11 @@
-<nav>
-    <ul>
-        <?php foreach ($liens as $lien) {
-            $url = $root_url . $lien['url'];
-            $name = $lien['name'];
-            echo '<li><a href="' . $url . '">' . $name . '</a></li>';
-        } ?>
-    </ul>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container">
+        <ul class="navbar-nav">
+            <?php foreach ($liens as $lien) {
+                $url = $root_url . $lien['url'];
+                $name = $lien['name'];
+                echo '<li class="nav-item"><a class="nav-link" href="' . $url . '">' . $name . '</a></li>';
+            } ?>
+        </ul>
+    </div>
 </nav>
