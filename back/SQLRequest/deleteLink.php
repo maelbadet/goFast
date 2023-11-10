9c6c37a3e17e4eba9fc668ec00bfd9f410085659
maelbadet <maelbadet21@gmail.com>
2023-11-10 16:41:28 +0100

    fix: add state on url

--- back/SQLRequest/deleteLink.php
+++ back/SQLRequest/deleteLink.php
@@ -1,48 +1,57 @@
-<?php include_once('../../front/partials/header.php'); ?>
+<?php
+session_start();
 
-<div class="container mt-5">
-    <div class="row justify-content-center">
-        <div class="col-md-6">
-            <?php
-            if (isset($_POST['submit'])) {
-                $userId = isset($_SESSION['id']) ? $_SESSION['id'] : null;
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    if (isset($_POST['action'], $_POST['lien_a_supprimer'])) {
+        $userId = isset($_SESSION['id']) ? $_SESSION['id'] : null;
 
-                if ($userId !== null) {
-                    $database = new mysqli("localhost", "root", "", "gofast");
+        if ($userId !== null) {
+            $database = new mysqli("localhost", "root", "", "gofast");
 
-                    $lien_a_supprimer = $_POST['lien_a_supprimer'];
+            $lien_a_supprimer = $_POST['lien_a_supprimer'];
+            $action = $_POST['action'];
 
-                    $query = $database->prepare("DELETE FROM url WHERE id = ? AND utilisateur_id = ?");
-                    $query->bind_param('ii', $lien_a_supprimer, $userId);
+            if ($action === 'disable') {
+                $query = $database->prepare("UPDATE url SET etat = true WHERE id = ? AND utilisateur_id = ?");
+            } elseif ($action === 'delete') {
+                $query = $database->prepare("DELETE FROM url WHERE id = ? AND utilisateur_id = ?");
+            }
 
-                    if ($query->execute()) {
-                        ?>
-                        <div class="alert alert-success" role="alert">
-                            Le lien a été supprimé avec succès.
-                        </div>
-                        <button class="btn btn-primary"><a href="../../index.php" class="text-white">Accueil</a></button>
-                        <?php
-                    } else {
-                        ?>
-                        <div class="alert alert-danger" role="alert">
-                            Erreur lors de la suppression du lien : <?= $database->error ?>
-                        </div>
-                        <?php
-                    }
+            if (isset($query)) {
+                $query->bind_param('ii', $lien_a_supprimer, $userId);
 
-                    $query->close();
-                    $database->close();
+                if ($query->execute()) {
+                    ?>
+                    <div class="alert alert-success mt-3" role="alert">
+                        <?php echo ($action === 'disable') ? "Le lien a été désactivé avec succès." : "Le lien a été supprimé avec succès."; ?>
+                    </div>
+                    <a href="../../index.php" class="btn btn-primary mt-3">Accueil</a>
+                    <?php
                 } else {
                     ?>
-                    <div class="alert alert-danger" role="alert">
-                        Erreur : Identifiant de session non disponible.
+                    <div class="alert alert-danger mt-3" role="alert">
+                        Erreur lors de <?php echo ($action === 'disable') ? "la désactivation" : "la suppression"; ?> du lien : <?= $database->error ?>
                     </div>
                     <?php
                 }
-            }
-            ?>
-        </div>
-    </div>
-</div>
 
-<?php include_once('../../front/partials/footer.php'); ?>
+                $query->close();
+            } else {
+                ?>
+                <div class="alert alert-danger mt-3" role="alert">
+                    Action non valide.
+                </div>
+                <?php
+            }
+
+            $database->close();
+        } else {
+            ?>
+            <div class="alert alert-danger mt-3" role="alert">
+                Erreur : Identifiant de session non disponible.
+            </div>
+            <?php
+        }
+    }
+}
+?>
