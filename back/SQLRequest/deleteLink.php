5c00103ba7ca7d8ac40a1ab1bafbde824d5700d3
maelbadet <maelbadet21@gmail.com>
2023-11-10 15:33:53 +0100

    fix: add style to all page

--- back/SQLRequest/deleteLink.php
+++ back/SQLRequest/deleteLink.php
@@ -1,27 +1,48 @@
-<?php
-include_once('../../front/partials/header.php');
+<?php include_once('../../front/partials/header.php'); ?>
 
-if (isset($_POST['submit'])) {
-    $userId = isset($_SESSION['id']) ? $_SESSION['id'] : null;
+<div class="container mt-5">
+    <div class="row justify-content-center">
+        <div class="col-md-6">
+            <?php
+            if (isset($_POST['submit'])) {
+                $userId = isset($_SESSION['id']) ? $_SESSION['id'] : null;
 
-    if ($userId !== null) {
-        $database = new mysqli("localhost", "root", "", "gofast");
-        
-        $lien_a_supprimer = $_POST['lien_a_supprimer'];
-        
-        $query = $database->prepare("DELETE FROM url WHERE id = ? AND utilisateur_id = ?");
-        $query->bind_param('ii', $lien_a_supprimer, $userId);
-        
-        if ($query->execute()) {
-            echo "Le lien a été supprimé avec succès.";
-            ?><button><a href="../../index.php"> Accueil</a></button><?php
-        } else {
-            echo "Erreur lors de la suppression du lien : " . $database->error;
-        }
-        
-        $database->close();
-    } else {
-        echo "Erreur : Identifiant de session non disponible.";
-    }
-}
-?>
+                if ($userId !== null) {
+                    $database = new mysqli("localhost", "root", "", "gofast");
+
+                    $lien_a_supprimer = $_POST['lien_a_supprimer'];
+
+                    $query = $database->prepare("DELETE FROM url WHERE id = ? AND utilisateur_id = ?");
+                    $query->bind_param('ii', $lien_a_supprimer, $userId);
+
+                    if ($query->execute()) {
+                        ?>
+                        <div class="alert alert-success" role="alert">
+                            Le lien a été supprimé avec succès.
+                        </div>
+                        <button class="btn btn-primary"><a href="../../index.php" class="text-white">Accueil</a></button>
+                        <?php
+                    } else {
+                        ?>
+                        <div class="alert alert-danger" role="alert">
+                            Erreur lors de la suppression du lien : <?= $database->error ?>
+                        </div>
+                        <?php
+                    }
+
+                    $query->close();
+                    $database->close();
+                } else {
+                    ?>
+                    <div class="alert alert-danger" role="alert">
+                        Erreur : Identifiant de session non disponible.
+                    </div>
+                    <?php
+                }
+            }
+            ?>
+        </div>
+    </div>
+</div>
+
+<?php include_once('../../front/partials/footer.php'); ?>
--- front/crud/update.php
+++ front/crud/update.php
@@ -1,11 +1,20 @@
 <?php include_once('../../front/partials/header.php'); ?>
-
 <?php require_once('../../back/SQLRequest/listing.php'); ?>
-<h1> Modification d'un lien</h1>
 
-<form action="../form/update-form.php" method="post">
-    <select name="lien_a_modifier">
-        <?php echo implode("\n", $options); ?>
-    </select>
-    <input type="submit" name="submit_update" value="Modifier">
-</form>
+<h1>Modification d'un lien</h1>
+
+<div class="container mt-5">
+    <div class="row justify-content-center">
+        <div class="col-md-6">
+            <form action="../form/update-form.php" method="post" class="mb-3">
+                <div class="mb-3">
+                    <label for="lien_a_modifier" class="form-label">Sélectionnez le lien à modifier :</label>
+                    <select name="lien_a_modifier" id="lien_a_modifier" class="form-select">
+                        <?php echo implode("\n", $options); ?>
+                    </select>
+                </div>
+                <input type="submit" name="submit_update" value="Modifier" class="btn btn-primary">
+            </form>
+        </div>
+    </div>
+</div>
