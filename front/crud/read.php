a7997cfabf83655fec8cab0218ccec3822f853e4
Clément Gaubert <clement.gaubert@livecampus.tech>
2023-11-12 14:10:12 +0100

    tableau url désac

--- front/crud/read.php
+++ front/crud/read.php
@@ -3,7 +3,8 @@
 <?php
 
     $database = new mysqli("localhost", "root", "", "gofast");
-    $query = $database->query("SELECT * FROM url WHERE utilisateur_id = 1"); //remplacer utilisateur_id = 1 par utilisateur courant
+    $query = $database->query("SELECT * FROM url WHERE etat = 0"); //requete url valide
+    $query_disable = $database->query("SELECT * FROM url WHERE etat = 1"); //requete url desactivé
 
     if ($query)
     {
@@ -15,24 +16,38 @@
     } else {
     echo "Erreur dans la requête SQL : " . $database->error;
     }
+
+
+    if ($query_disable)
+    {
+        $data_disable = array();
+        while($row_disable = $query_disable->fetch_assoc())
+        {
+            $data_disable[] = $row_disable;
+        }
+    } else {
+    echo "Erreur dans la requête SQL : " . $database->error;
+    }
+
     $database->close();
+    
 ?>
 
-<p>
-    Ici, il faut pouvoir lire les différents liens déja mis en places
-</p>
+</br>
+<center>
+<h1>
+    Différent(s) lien(s) activé(s)
+</h1>
+</center>
+</br>
 
-
-<table>
-    <thead>
+<table class="table table-striped table-dark">
+  <thead>
         <tr>
-            <th colspan="4">Différent(s) lien(s)</th>
-        </tr>
-        <tr>
-            <td>Id</td>
-            <td>Lien</td>
-            <td>Lien raccourci</td>
-            <td>Click</td>
+            <th scope="col">ID</th>
+            <th scope="col">Lien</th>
+            <th scope="col">Lien raccourci</th>
+            <th scope="col">Click</th>
         </tr>
     </thead>
     <tbody>
@@ -40,8 +55,8 @@
         foreach ($data as $row): ?>
         <tr>
             <td><?= $row['id'] ?></td>
-            <td><?= $row['link'] ?></td>
-            <td><?= $row['link_rewrite'] ?></td>
+            <td><a href=<?= $row['link'] ?> target="_blank"> <?= $row['link'] ?></a></td>
+            <td><a href="<?= $root_url ?>back\processing\redirect.php?short=<?= $row['link_rewrite'] ?>" target="_blank\"><?= $row['link_rewrite'] ?></a></td>
             <td><?= $row['click'] ?></td>
         </tr>
         <?php endforeach; 
@@ -49,13 +64,33 @@
     </tbody>
 </table>
 
-<style>
-    table, td {
-        border: 1px solid #333;
-    }
+</br>
+<center>
+<h1>
+    Différent(s) lien(s) désactivé(s)
+</h1>
+</center>
+</br>
 
-    thead,tfoot {
-        background-color: #333;
-        color: #fff;
-    }
-</style>
+<table class="table table-striped table-dark">
+  <thead>
+        <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Lien</th>
+            <th scope="col">Lien raccourci</th>
+            <th scope="col">Click</th>
+        </tr>
+    </thead>
+    <tbody>
+    <?php
+        foreach ($data_disable as $row_disable): ?>
+        <tr>
+            <td><?= $row_disable['id'] ?></td>
+            <td><a href=<?= $row_disable['link'] ?> target="_blank"> <?= $row_disable['link'] ?></a></td>
+            <td><a href="<?= $root_url ?>back\processing\redirect.php?short=<?= $row_disable['link_rewrite'] ?>" target="_blank\"><?= $row_disable['link_rewrite'] ?></a></td>
+            <td><?= $row_disable['click'] ?></td>
+        </tr>
+        <?php endforeach; 
+    ?>
+    </tbody>
+</table>
